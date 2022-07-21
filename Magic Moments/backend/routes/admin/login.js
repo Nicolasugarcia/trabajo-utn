a5a02b98c37158eefd6d11b9ceb9aab1c6c6f28e
Nicolas Garcia <nicolasgarciasam@gmail.com>
2022-07-20 23:29:58 -0300

    correcion

--- Magic Moments/backend/routes/admin/login.js
+++ Magic Moments/backend/routes/admin/login.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render("admin/login", {
-    layout: "admin/login"
+    layout: "admin/layout"
   });
 });
 
