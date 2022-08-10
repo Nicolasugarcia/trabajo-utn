b3b146bf18d53dd369e4efead838f84a9caa4ab3
Nicolas Garcia <nicolasgarciasam@gmail.com>
2022-08-09 22:28:09 -0300

    entrega

--- M53/app.js
+++ M53/app.js
@@ -5,7 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 require("dotenv").config();
-var pool = require("./models/bs");
+var pool = require("./models/Bd");
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
