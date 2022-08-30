fc4ad046a44b2c6b49a5d515225956b012fdc66d
Nicolas Garcia <nicolasgarciasam@gmail.com>
2022-08-29 22:18:33 -0300

    contacto

--- Magic Moments/backend/routes/api.js
+++ Magic Moments/backend/routes/api.js
@@ -2,6 +2,7 @@ var express = require("express");
 var router = express.Router();
 var combosModel = require ("./../models/combosModel")
 var cloudinary = require("cloudinary").v2;
+var nodemailer= require("nodemailer");
 
 router.get("/combos", async function (req, res, next){
   let combos = await combosModel.getCombos();
@@ -26,6 +27,31 @@ router.get("/combos", async function (req, res, next){
   })
 
   res.json(combos);
+});
+
+
+router.post("/contacto", async (req, res)=>{
+    const mail={
+        to: "nicolasgarciasam@gmail.com",
+        subject: "contacto web",
+        html: `${req.body.nombre} ${req.body.apellido} se conecto a traves de la web y quiero coordinar su pedido en este correo:
+        ${req.body.mail}<br> Ademas sugerio el dia ${req.body.fecha} con horario ${req.body.time}<br> Su numero de contacto es ${req.body.telefono} ` 
+    }
+    const transport = nodemailer.createTransport({
+        host:process.env.SMTP_HOST,
+        port:process.env.SMTP_PORT,
+        auth: {
+            user:process.env.SMTP_USER,
+            pass:process.env.SMTP_PASS
+        }
+    });
+    
+    await transport.sendMail(mail)
+
+    res.status(201).json({
+        error: false,
+        message: "Mensaje enviado"
+    });
 });
 
 module.exports= router;
